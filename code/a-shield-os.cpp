b797841458ea691cef55207559698f9183a55e2d
John Zygoulakis <67337564+johnzgl00@users.noreply.github.com>
2021-07-23 12:23:27 +0000

    Add files via upload

--- code/a-shield-os.cpp
+++ code/a-shield-os.cpp
@@ -3,7 +3,19 @@
 #include <stdlib.h>;
 using namespace std;
 
+//Colors
+string colorRed = "\e[1;31m";
+string colorYellow = "\e[1;33m";
+string colorGreen = "\e[1;32m";
+string noColor = "\e[0m";
+string colorMagenta = "\e[1;35m";
+string colorCyan = "\e[1;36m";
+string colorBlue = "\e[1;34m";
+//Variables
 string command ;
+string username = "A-shield";
+string deviceId = "0x01";
+string platform = "Raspberry";
 
 void start_msg() {
 	cout << "****************************\n";
@@ -13,20 +25,28 @@ void start_msg() {
 }
 
 void get_command_msg() {
-	cout << "A-shield@user(/): ";
+	cout << colorYellow + "A-shield" + colorBlue + "@" + colorYellow + "user" + colorRed + "(/): " + noColor;
 	getline(cin, command);
 }
 
 void check_command() {
 	if (command == "whoami") {
-		cout << "user";
+		cout << "username: " + username;
+	}
+	else if (command == "") {
+		//Do nothing
 	}
 	else if (command == "device -id") {
-		cout << "0x65";
+		cout << "Device Info\n";
+		cout << "	id: " + deviceId + "\n";
+		cout << "	platform: " + platform;
 	}
 	else if (command == "power-down") {
 		cout << "Stopping proccesses...";
 		exit(1);
+	}
+	else if (command == "proccess --kill") {
+		cout << "Killing proccesses...";
 	}
 	else {
 		cout << "Err: command not found (code:404)";

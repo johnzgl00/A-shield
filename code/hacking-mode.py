3fa82d5355278d7dcde684f7345276f1d216e752
John Zygoulakis <67337564+johnzgl00@users.noreply.github.com>
2021-07-25 08:53:24 +0000

    Add files via upload

--- code/hacking-mode.py
+++ code/hacking-mode.py
@@ -1,1 +1,17 @@
-print("Hello World")
+import tkinter as tk
+
+window = tk.Tk()
+window.geometry("420x360")
+window.title("Hacking Mode")
+window.configure(bg="grey12")
+
+attack_btn = tk.Button(bg="yellow")
+attack_btn.pack()
+
+reccon_btn = tk.Button(bg="blue")
+reccon_btn.pack()
+
+exit_btn = tk.Button(bg="red")
+exit_btn.pack()
+
+window.mainloop()
